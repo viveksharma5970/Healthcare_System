e0e1ba6371fbf53f640fdbb5bf24351ae443f863
Vivek Sharma <viveksharma5970@gmail.com>
2025-10-31 18:55:46 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000', // for local dev
-    },
-  },
   build: {
-    outDir: '../backend/client', // where frontend build will go
-  },
+    outDir: '../backend/client',
+    emptyOutDir: true
+  }
 })
