7e315e4daf8ec941b6a51dbb8f00fb9135ef72f1
Vivek Sharma <viveksharma5970@gmail.com>
2025-11-01 11:28:28 +0530

    database fix

--- server/src/lib/database.lib.js
+++ server/src/lib/database.lib.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 
-    const DATABASE_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wpoesbf.mongodb.net/?appName=Cluster0`
+    const DATABASE_URL = `mongodb+srv://viveksharma5970_db_user:${process.env.DB_PASSWORD}@cluster0.wpoesbf.mongodb.net/?appName=Cluster0`
     try {
         const connection = await mongoose.connect(`${DATABASE_URL}`);
         console.log(`MongoDB connected: ${connection.connection.host}`);
