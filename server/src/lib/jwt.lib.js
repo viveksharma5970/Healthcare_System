ee5ff94c645be7b1a8a62ae65f316f31f6bf7c86
Vivek Sharma <viveksharma5970@gmail.com>
2025-11-01 11:40:31 +0530

    fix cookie bug

--- server/src/lib/jwt.lib.js
+++ server/src/lib/jwt.lib.js
@@ -7,7 +7,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV === "production",
   });
   return token;
